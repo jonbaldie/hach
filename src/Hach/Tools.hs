b1618853448a45bccc500f0b762cc9b742e27a04
Jonathan Baldie <jon@jonbaldie.com>
2026-09-06 15:45:05 +0100

    chore(release): bump version to 0.1.1 (#13)

--- src/Hach/Tools.hs
+++ src/Hach/Tools.hs
@@ -1390,7 +1390,7 @@ executeWebFetch (WebFetchArgs url) = do
       case reqRes of
         Left ex -> pure $ ToolError ("Invalid URL '" <> url <> "': " <> T.pack (show ex))
         Right req -> do
-          let req' = req { requestHeaders = [("User-Agent", "hach/0.1.0.0")] }
+          let req' = req { requestHeaders = [("User-Agent", "hach/0.1.1.0")] }
           respRes <- try (httpLbs req' mgr) :: IO (Either SomeException (Response BSL.ByteString))
           case respRes of
             Left ex -> pure $ ToolError ("HTTP fetch error: " <> T.pack (show ex))
